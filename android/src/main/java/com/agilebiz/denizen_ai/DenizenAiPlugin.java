c84a5eccfcd7ea3fe680dc6c393d3b8873ea000d
Ibrahim Karisa Fadhili <ibrahim.fadhili@agilebiz.co.ke>
2026-08-05 11:21:35 +0300

    fix(android): register denizen_ai as Android Flutter plugin to compile libsqlite_vec.so

---
+++ android/src/main/java/com/agilebiz/denizen_ai/DenizenAiPlugin.java
@@ -0,0 +1,15 @@
+package com.agilebiz.denizen_ai;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
+/** DenizenAiPlugin */
+public class DenizenAiPlugin implements FlutterPlugin {
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+  }
+}
